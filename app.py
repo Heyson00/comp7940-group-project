9b1002a8790a0eca1369d0a9cd204349e37c7598
krammer <931023602@qq.com>
2024-03-28 22:35:01 +0800

    feat(app.py)Host changed

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ class HKBU_GPT():
 #     return port
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=9000)
     # local_port = find_local_port()
     # print("Local port:", local_port) 
     
