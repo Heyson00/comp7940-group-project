d2a8e70df632ff1f249b7b7e689e674616d04fcf
ACzyx <783496219@qq.com>
2024-03-28 16:28:57 +0800

    Debug4

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ class HKBU_GPT():
 #     return port
 
 if __name__ == '__main__':
-    app.run(host="https://ggbond.zeabur.app")
+    app.run(host="34.49.168.86")
     # local_port = find_local_port()
     # print("Local port:", local_port) 
     
